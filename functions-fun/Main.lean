06cb13d383885229565814bd0ed7b58a8ce09340
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-04 19:06:45 -0800

    fun with functions

--- functions-fun/Main.lean
+++ functions-fun/Main.lean
@@ -1,6 +1,18 @@
 def add1 (n : Nat) : Nat := n + 1
 
+def my_max (n : Nat) (k : Nat) : Nat :=
+  if n < k then
+    k
+  else n
+
+def my_min (n : Nat) (k : Nat) : Nat :=
+  if n > k then
+    k
+  else n
+
 def main : IO Unit := IO.println "Hello World"
 #eval main
 
 #eval add1 5
+#eval my_max (10+20) (2*23)
+#eval my_min 1 3
