7a9f1682bb6d912ee431f3e9c4fb3ba19cf173f8
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-13 00:12:07 -0800

    theorem

---
+++ theorem-fun/Main.lean
@@ -0,0 +1,9 @@
+def main : IO Unit := IO.println "Hello World"
+#eval main
+
+theorem test (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  exact hp
+  apply And.intro
+  exact hq
+  exact hp
